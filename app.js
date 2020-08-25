82714039cbd04b8ae4fd4089335d4026da5b87d0
Sonugopal <sonugopal41@gmail.com>
2020-08-26 00:09:55 +0530

    Designed layout for admin and user

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
+var hbs=require('express-handlebars');
+var indexRouter = require('./routes/admin');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -12,7 +12,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+app.engine('hbs',hbs({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname + '/views/layout/',partialsDir:__dirname + '/views/partials/'}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
